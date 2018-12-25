036f6e3ce8409f459be2ffabdb513769c6c5af91
박세은 <pkse1213@naver.com>
2018-12-25 18:59:49 +0900

    follow 리사이클러뷰 아이템 구현

---
+++ app/src/main/java/com/example/parkseeun/moca_android/ui/follow/FollowActivity.kt
@@ -0,0 +1,13 @@
+package com.example.parkseeun.moca_android.ui.follow
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.parkseeun.moca_android.R
+
+class FollowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_follow)
+    }
+}
---
+++ app/src/main/java/com/example/parkseeun/moca_android/ui/follow/FollowData.kt
@@ -0,0 +1,3 @@
+package com.example.parkseeun.moca_android.ui.follow
+
+data class FollowData(var imageUrl: String, var name: String, var followFlag: Int)
