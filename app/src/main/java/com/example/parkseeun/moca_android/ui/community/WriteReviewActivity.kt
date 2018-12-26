673d9b4f9e666b59d8b0a8f107eec5f3ff807db4
Sohee <shfksthgml@naver.com>
2018-12-26 11:46:43 +0900

    2018/12/26 시작

---
+++ app/src/main/java/com/example/parkseeun/moca_android/ui/community/WriteReviewActivity.kt
@@ -0,0 +1,13 @@
+package com.example.parkseeun.moca_android.ui.community
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.parkseeun.moca_android.R
+
+class WriteReviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_community_writereview)
+    }
+}
