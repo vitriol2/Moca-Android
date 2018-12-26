79a977eba63248bddd0881f2f1af31286d485f8e
sovovy <sovovy@gmail.com>
2018-12-26 21:33:23 +0900

    <리뷰 상세보기> xml 수정 및 Class 이름 변경

--- app/src/main/java/com/example/parkseeun/moca_android/ui/review_detail/ReviewDetailActivity.kt
+++ app/src/main/java/com/example/parkseeun/moca_android/ui/review_detail/ReviewDetailActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.parkseeun.moca_android.R
 
-class MainActivity : AppCompatActivity() {
+class ReviewDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
