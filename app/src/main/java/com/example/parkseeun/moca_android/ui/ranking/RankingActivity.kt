27634d25fc0f423f869de300ef250b0b04c09a3b
sovovy <sovovy@gmail.com>
2019-01-01 19:23:45 +0900

    <리뷰 상세보기> 사진 비율 수정. <랭킹> 뷰 초기화

---
+++ app/src/main/java/com/example/parkseeun/moca_android/ui/ranking/RankingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.parkseeun.moca_android.ui.ranking
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.parkseeun.moca_android.R
+
+class RankingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ranking)
+    }
+}
