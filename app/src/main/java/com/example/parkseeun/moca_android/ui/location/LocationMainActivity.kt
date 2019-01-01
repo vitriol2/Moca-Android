2c39d61afd53ba0da47c16b438243b72f8e151a7
Sohee <shfksthgml@naver.com>
2019-01-02 01:26:05 +0900

    <LocationMainActivity> Daum->Google 변경

--- app/src/main/java/com/example/parkseeun/moca_android/ui/location/LocationMainActivity.kt
+++ app/src/main/java/com/example/parkseeun/moca_android/ui/location/LocationMainActivity.kt
@@ -1,41 +1,59 @@
 package com.example.parkseeun.moca_android.ui.location
 
-import android.app.Dialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.view.ViewGroup
-import android.widget.RelativeLayout
 import com.example.parkseeun.moca_android.R
-import com.example.parkseeun.moca_android.R.id.container
 import com.example.parkseeun.moca_android.ui.location.adapter.LocationMainAdapter
 import com.example.parkseeun.moca_android.ui.location.data.LocationMainData
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_location_main.*
-import net.daum.mf.map.api.MapView
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
-class LocationMainActivity : AppCompatActivity() {
-
+class LocationMainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
     // RecyclerView 설정
     lateinit var locationMainAdapter: LocationMainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location_main)
-        mapView()
+
         setRecyclerView()
+      googleMap()
         setOnBtnClickListener()
 
     }
 
-    fun mapView() {
-        var mapView : MapView = MapView(this)
-        mapView.setDaumMapApiKey("cb620091669ce5936f0fd4dd2766a760")
-        val container : RelativeLayout = findViewById(R.id.map_view)
-        container.addView(mapView)
-    }
+  fun googleMap(){
+      val mapFragment = supportFragmentManager
+          .findFragmentById(R.id.map) as SupportMapFragment
+      mapFragment.getMapAsync(this)
+  }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
     fun showLocationMainActToastMessage() {
         toast("로케이션 메인 엑티비티 함수 호출")
     }
