6f377b9193ce2569ec8956a92dae21ccf876e834
sovovy <sovovy@gmail.com>
2019-01-05 01:14:30 +0900

    <카테고리> 지역 사진 비율 4:5 유지, 지역 TextView 비율 맞춰서 적용, 마커 interaction 작용 구현

--- app/src/main/java/com/example/parkseeun/moca_android/ui/category/CategoryActivity.kt
+++ app/src/main/java/com/example/parkseeun/moca_android/ui/category/CategoryActivity.kt
@@ -7,12 +7,18 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import com.example.parkseeun.moca_android.R
 import kotlinx.android.synthetic.main.activity_category.*
+import android.support.constraint.ConstraintSet
+import android.widget.TextView
+
 
 class CategoryActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var menuViewAdapter : ButtonViewAdapter
     private lateinit var conceptViewAdapter : ButtonViewAdapter
     private var menuList : ArrayList<ButtonData> = ArrayList()
     private var conceptList : ArrayList<ButtonData> = ArrayList()
+    private lateinit var regionIds : ArrayList<Int>
+    private lateinit var regionTvs : ArrayList<TextView>
+    private var regionSelected : Int = -1
 
     override fun onClick(v: View?) {
         when(v){
@@ -35,15 +41,15 @@ class CategoryActivity : AppCompatActivity(), View.OnClickListener {
                 }
             }
         }
-        if(isChecked())
-            cate_next_iv.setTextColor(resources.getColor(R.color.colorPrimary))
-        else
-            cate_next_iv.setTextColor(resources.getColor(R.color.dark_gray))
+        setNextColor()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+
+        initRegion()
+        setClickListener()
 
         cate_next_iv.setOnClickListener(this)
 
@@ -81,9 +87,17 @@ class CategoryActivity : AppCompatActivity(), View.OnClickListener {
         cate_concept_rv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
     }
 
+    private fun setNextColor() {
+        if(isChecked())
+            cate_next_iv.setTextColor(resources.getColor(R.color.colorPrimary))
+        else
+            cate_next_iv.setTextColor(resources.getColor(R.color.dark_gray))
+    }
     // 카테고리 선택했는지 알려주는 함수
     private fun isChecked(): Boolean {
         // 지도 체크
+        if(regionSelected != -1)
+            return true
         // 메뉴 체크
         for(item in menuList)
             if(item.flag)
@@ -93,5 +107,42 @@ class CategoryActivity : AppCompatActivity(), View.OnClickListener {
             if(item.flag)
                 return true
         return false
+    }
+    // 지역 관련 변수 초기화
+    private fun initRegion() {
+        // 지역 TextView Id
+        // 강서 구로 양천 금천 영등포 마포 은평 서대문 관악 동작 종로 용산 중구 서초 강북 성북 도봉 성동 동대문 강남 노원 광진 중랑 송파 강동
+        regionIds = arrayListOf(R.id.gangseo_tv, R.id.guro_tv, R.id.yangcheon_tv, R.id.goldcheon_tv, R.id.zeropo_tv, R.id.mapo_tv, R.id.silver_tv, R.id.seodaemoon_tv,
+                R.id.pipe_tv, R.id.action_tv, R.id.jongro_tv, R.id.dragon_tv, R.id.midnine_tv, R.id.seocho_tv, R.id.gangbook_tv,
+                R.id.seongbook_tv, R.id.dobong_tv, R.id.seongdong_tv, R.id.dongdae_tv, R.id.gangnam_tv, R.id.noone_tv, R.id.gwangjin_tv,
+                R.id.midrang_tv, R.id.songpa_tv, R.id.gangdong_tv)
+        regionTvs = arrayListOf(gangseo_tv, guro_tv, yangcheon_tv, goldcheon_tv, zeropo_tv, mapo_tv, silver_tv, seodaemoon_tv,
+                pipe_tv, action_tv, jongro_tv, dragon_tv, midnine_tv, seocho_tv, gangbook_tv,
+                seongbook_tv, dobong_tv, seongdong_tv, dongdae_tv, gangnam_tv, noone_tv, gwangjin_tv,
+                midrang_tv, songpa_tv, gangdong_tv)
+    }
+    // 지역 TextView에 clickListener 달기
+    private fun setClickListener() {
+        for(i in 0 until regionTvs.size){
+            regionTvs[i].setOnClickListener {
+                // 이전에 선택한 지역이 클릭한 지역일 때
+                if(regionSelected != -1 && regionSelected == i) {
+                    regionSelected = -1
+                    cate_marker_iv.visibility = View.GONE
+                }else{
+                    // 이전에 선택한 지역과 다른 지역 혹은 처음으로 지역을 선택할 때
+                    regionSelected = i
+                    // 마커 위치 설정: 선택한 지역의 id를 기준으로 설정
+                    val constraintSet = ConstraintSet()
+                    constraintSet.clone(cate_map_const)
+                    constraintSet.connect(R.id.cate_marker_iv, ConstraintSet.START, regionIds[i], ConstraintSet.START)
+                    constraintSet.connect(R.id.cate_marker_iv, ConstraintSet.END, regionIds[i], ConstraintSet.END)
+                    constraintSet.connect(R.id.cate_marker_iv, ConstraintSet.BOTTOM , regionIds[i], ConstraintSet.TOP)
+                    constraintSet.applyTo(cate_map_const)
+                    cate_marker_iv.visibility = View.VISIBLE
+                }
+                setNextColor()
+            }
+        }
     }
 }
