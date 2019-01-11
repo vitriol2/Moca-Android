46cd55026be113a637873d535c8167d7e92696a0
vitriol2 <vitriol0919@gmail.com>
2019-01-12 07:26:33 +0900

    nearbyListActivity complete

--- app/src/main/java/com/example/parkseeun/moca_android/ui/detail/nearbyList/NearbyListAdapter.kt
+++ app/src/main/java/com/example/parkseeun/moca_android/ui/detail/nearbyList/NearbyListAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.parkseeun.moca_android.ui.detail.nearbyList
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Point
 import android.support.v7.widget.RecyclerView
 import android.util.Log
@@ -10,10 +11,12 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.RatingBar
+import android.widget.RelativeLayout
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.example.parkseeun.moca_android.R
 import com.example.parkseeun.moca_android.model.post.PostNearByCafeResponseData
+import com.example.parkseeun.moca_android.ui.detail.DetailActivity
 
 class NearbyListAdapter(val context : Context, val dataList : ArrayList<PostNearByCafeResponseData>) : RecyclerView.Adapter<NearbyListAdapter.Holder>() {
 
@@ -28,6 +31,12 @@ class NearbyListAdapter(val context : Context, val dataList : ArrayList<PostNear
 
     override fun onBindViewHolder(holder: NearbyListAdapter.Holder, position: Int) {
         // 스크린 너비에 따른 이미지 길이 설정
+        holder.item.setOnClickListener {
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra("cafe_id", dataList[position].cafe_id)
+            context.startActivity(intent)
+        }
+
         val width = getScreenWidth()
         holder.pic.layoutParams.width = ((width-dpToPx(40.toFloat()))).toInt()
         holder.pic.layoutParams.height = holder.pic.layoutParams.width-20
@@ -43,6 +52,7 @@ class NearbyListAdapter(val context : Context, val dataList : ArrayList<PostNear
 
     // View Holder
     inner class Holder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val item : RelativeLayout = itemView.findViewById(R.id.rl_rv_act_nearby_list_item) as RelativeLayout
         val pic : ImageView = itemView.findViewById(R.id.iv_rv_act_nearby_list_item) as ImageView
         val cafename : TextView = itemView.findViewById(R.id.tv_rv_act_nearby_list_name) as TextView
         val location : TextView = itemView.findViewById(R.id.tv_rv_act_nearby_list_location) as TextView
